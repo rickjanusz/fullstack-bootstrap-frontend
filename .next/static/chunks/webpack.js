bf2782330928f84261a1bb48497aa686e4641cb3
Rick Janusz <rickjanusz@gmail.com>
2021-02-09 15:30:57 -0600

    adding crud/delete item using apollo3 cache.evict

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "89b825582c98d31ccac4";
+/******/ 	var hotCurrentHash = "8e9683c3e8321a52b028";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
