8d47df2ff4eab5e53c5b43880a0b2502044ab08d
Rick Janusz <rickjanusz@gmail.com>
2021-02-17 12:31:31 -0600

    permissions cont, roles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "336fc106af22d8697e9f";
+/******/ 	var hotCurrentHash = "2551e11b53b9b917b77c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
